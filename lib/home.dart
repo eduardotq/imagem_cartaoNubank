dd84059947961a4a4d57e0310e6775ddc179261c
Eduardo Rodrigues <lz.eduardo2006@gmail.com>
2020-07-03 14:15:44 -0300

    retirada SafeArea

--- lib/home.dart
+++ lib/home.dart
@@ -12,32 +12,30 @@ class ImagemNubank extends StatefulWidget {
 class _ImagemNubankState extends State<ImagemNubank> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(109, 33, 119, 1),
-          title: Text('Cartão do Nubank'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.credit_card, color: Colors.white),
-              onPressed: null,
-            )
-          ],
-        ),
-        backgroundColor: Color.fromRGBO(153, 51, 153, .5),
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlipCard(
-              direction: FlipDirection.HORIZONTAL, // default
-              front: FrenteCartao(),
-              back: Container(
-                child: AtrasCartao(),
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(109, 33, 119, 1),
+        title: Text('Cartão do Nubank'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.credit_card, color: Colors.white),
+            onPressed: null,
+          )
+        ],
+      ),
+      backgroundColor: Color.fromRGBO(153, 51, 153, .5),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlipCard(
+            direction: FlipDirection.HORIZONTAL, // default
+            front: FrenteCartao(),
+            back: Container(
+              child: AtrasCartao(),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
